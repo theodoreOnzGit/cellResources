73983ee2b482fa2ff3067e8fc43b6e6e54ff73c6
theoOng <fifadude114@gmail.com>
2020-09-01 12:03:40 -0700

    template Gen adn clean bash scripts added

---
+++ iceBreaker/templateGen.sh
@@ -0,0 +1,13 @@
+#this file is to help generate instructions, questions and answers for small group games
+
+echo "This is the instruction file for participants ... " >> instructionFile.txt
+
+echo "1) Grouping instructions (please state how members will group themselves)" >> instructionFile.txt
+
+
+echo "2) Game instructions (please state steps for the game)" >> instructionFile.txt
+
+echo "This is the question paper file ..." >> questionPaper.txt
+
+echo "This is the game master's answer sheet... " >> gameMasterAnswer.txt
+
