bd95e732d2a974b6b71c4b34dffbab0b445103f8
theoOng <fifadude114@gmail.com>
2020-09-01 12:46:55 -0700

    template file updated

--- iceBreaker/templateGen.sh
+++ iceBreaker/templateGen.sh
@@ -4,8 +4,18 @@ echo "This is the instruction file for participants ... " >> instructionFile.txt
 
 echo "1) Grouping instructions (please state how members will group themselves)" >> instructionFile.txt
 
+echo "2) Web Resources and other materials... (please state how zoom, electronic devices or other websites are used)" >> instructionFile.txt
 
-echo "2) Game instructions (please state steps for the game)" >> instructionFile.txt
+echo "3) Physical materials (please state what physical materials participants need to get ready" >> instructionFile.txt
+
+echo "4) Game instructions (please state steps for the game)" >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+
+echo "Last Step: Contingency Plan... (what if something goes wrong, wet weather etc)" >> instructionFile.txt
+
+echo "Reviews: After playing a round of games, how did the game go and what changes do we need to make" >> instructionFile.txt
+
 
 echo "This is the question paper file ..." >> questionPaper.txt
 
