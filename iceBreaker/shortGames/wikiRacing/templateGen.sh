cd4336f4a795fec7350c2d94826edd1a996e1c98
Ted <fifadude114@gmail.com>
2021-02-04 16:13:41 -0800

    murder mystery updated

---
+++ iceBreaker/shortGames/wikiRacing/templateGen.sh
@@ -0,0 +1,45 @@
+#this file is to help generate instructions, questions and answers for small group games
+
+echo "This is the instruction file for participants ... " >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+
+echo "Game Summary: please state what the game is in one sentence for participants" >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+echo "1) Grouping instructions (please state how members will group themselves)" >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+
+
+echo "2) Web Resources and other materials... (please state how zoom, electronic devices or other websites are used)" >> instructionFile.txt
+
+
+echo "" >> instructionFile.txt
+
+
+echo "3) Physical materials (please state what physical materials participants need to get ready" >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+
+
+echo "4) Game instructions (please state steps for the game)" >> instructionFile.txt
+
+echo "" >> instructionFile.txt
+
+echo "5) Time Requirement (ice breakers tend to be time sensitive, so state roughly how long the game will take)" >> instructionFile.txt
+
+
+echo "" >> instructionFile.txt
+
+echo "Last Step: Contingency Plan... (what if something goes wrong, wet weather etc)" >> instructionFile.txt
+
+echo "Reviews: After playing a round of games, how did the game go and what changes do we need to make" >> instructionFile.txt
+
+
+echo "This is the question paper file ..." >> questionPaper.txt
+
+echo "This is the game master's answer sheet... " >> gameMasterAnswer.txt
+
+
+
